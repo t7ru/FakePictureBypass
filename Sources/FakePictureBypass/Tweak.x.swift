68898d3eb1cda8750b691c655d505be5c1a9b196
tofu <57356716+t7ru@users.noreply.github.com>
2025-01-17 20:51:43 -0500

    something

--- Sources/FakePictureBypass/Tweak.x.swift
+++ Sources/FakePictureBypass/Tweak.x.swift
@@ -1,2 +1,13 @@
 import Orion
+import UIKit
 import FakePictureBypassC
+
+class TableHook: ClassHook<UITableViewLabel> {
+    func setText(_ text: String) {
+        if text.contains("Settings") {
+            orig.setText("Settings xd")
+        } else {
+        orig.setText(text)
+        }
+    }
+}
